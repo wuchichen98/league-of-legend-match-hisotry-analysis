3e0d3cb5fd803a929bd08a33d93fdf045c72a783
s3695116 <41458671+s3695116@users.noreply.github.com>
2020-05-22 14:23:17 +0800

    done

--- commentBook.php
+++ commentBook.php
@@ -19,16 +19,7 @@ if (array_key_exists('content', $_POST) && $_SESSION['fname']!=null) {
   echo "<meta http-equiv='refresh' content='0'>";
 }
 
-// else {
-//   echo 'Please login before comment';
-//   echo '<script>window.location.href="./login.php"</script>';
-// }
 
-//could be
-//session_destroy();
-
-
-// print_r($info);
 
 ?>
 <div class="continer" >
@@ -39,8 +30,6 @@ if (array_key_exists('content', $_POST) && $_SESSION['fname']!=null) {
   <?php 
  
   for($i=count($info)-1; $i>=0; $i--){
-    // echo $info[$i]['name'];
-    // echo $info[$i]['comment'];
     echo '
       <div class="panel">
         <div class="panel-head" style="background-color: rgb(223,240,216)">
--- register.php
+++ register.php
@@ -10,7 +10,7 @@ if (array_key_exists('username', $_POST) && array_key_exists('password', $_POST)
     $lname =$_POST['lname'];
 
 
-    if (insertRow($username,$password, $fname,$lname)===true){
+    if (insertRow($username,$fname, $lname,$password)===true){
 	echo 'reg success';
 	echo ' redirecting.....';
 	echo '<script>window.location.href="./login.php"</script>';
@@ -23,37 +23,6 @@ if (array_key_exists('username', $_POST) && array_key_exists('password', $_POST)
 
 
 ?>
-
-
-
-<!-- <form method="post">
-		<fieldset>
-			<legend>UserLogin</legend>
-			<ul>
-				<li>
-					<label>User  ID:</label>
-					<input type="text" name="username" id="username">
-				</li>
-				<li>
-					<label>Password:</label>
-					<input type="password" name="password" id="password">
-				</li>
-                <li>
-					<label>User Firstname:</label>
-					<input type="text" name="fname" id="fname">
-				</li>
-                <li>
-					<label>User Lastname:</label>
-					<input type="text" name="lname" id="lname">
-				</li>
-				<li>
-					<label></label>
-					<input type="submit" name="submit" value="submit">
-				</li>
-			</ul>
-		</fieldset>
-	</form> -->
-
     <body class='login-body'>
 <form method="post" >
 <div class="box">
